a92dfd16eb751a4b6d660193fb55fd592f7e7b91
Brendan McCray <rbmccr@gmail.com>
2019-01-16 15:19:30 -0600

    corrected 13

--- line_item_track_artist.sql
+++ line_item_track_artist.sql
@@ -1,16 +1,5 @@
 -- Provide a query that includes the purchased track name AND artist name with each invoice line item
 
--- WRONG ... Ask about this!
--- SELECT InvoiceLine.InvoiceLineId, Track.Name, Artist.Name as "Artist Name"
--- FROM InvoiceLine
--- LEFT JOIN Track
--- ON InvoiceLine.TrackId = Track.TrackId
--- LEFT JOIN Album
--- ON Album.AlbumId = Track.AlbumId
--- LEFT JOIN Artist
--- ON Artist.ArtistId = Album.AlbumId
--- ORDER BY Artist.Name
-
 SELECT InvoiceLine.InvoiceLineId, Track.Name, Artist.Name as "Artist Name"
 FROM InvoiceLine
 LEFT JOIN Track
@@ -18,5 +7,7 @@ ON InvoiceLine.TrackId = Track.TrackId
 LEFT JOIN Album
 ON Album.AlbumId = Track.AlbumId
 LEFT JOIN Artist
-ON Artist.ArtistId = Album.AlbumId
-ORDER BY InvoiceLine.InvoiceLineId
+ON Artist.ArtistId = Album.ArtistId
+ORDER BY Artist.Name
+
+-- returns 2240 lines
